338772ec695806d8dcce54c224b8435a90f546df
zephyrdark <zephyrdark@users.noreply.github.com>
2024-07-12 23:43:51 +0800

    add: init project files

---
+++ src/main/java/com/coin/sa58_alvinlee_fibonacci/Sa58AlvinLeeFibonacciApplication.java
@@ -0,0 +1,13 @@
+package com.coin.sa58_alvinlee_fibonacci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa58AlvinLeeFibonacciApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sa58AlvinLeeFibonacciApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coin/sa58_alvinlee_fibonacci/Sa58AlvinLeeFibonacciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coin.sa58_alvinlee_fibonacci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa58AlvinLeeFibonacciApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
